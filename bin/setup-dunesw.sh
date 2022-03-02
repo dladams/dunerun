99b89c700e6fd9746c34779b489b3fda8f7810ce
David Adams <dladams@bnl.gov>
2022-03-02 16:38:52 +0000

    Support local builds in setup-dunesw.sh

--- bin/setup-dunesw.sh
+++ bin/setup-dunesw.sh
@@ -1,10 +1,63 @@
-VERS=$(echo $1 | sed 's/:.*//g')
-QUAL=${1:$((${#VERS}+1))}
-if [ -z "$QUAL" ]; then QUAL=e20:prof; fi
+# setup-dunesw.sh
+#
+# David Adams
+# March 2022
+#
+# Script to set up dunesw under different scenarios depending on the value
+# of the first positional argument REL. If this argument contains '/', it
+# is interpreted as a directory or file name. The action to take depends on
+# REL:
+# -- If a file name ending '.sh', the file is sourced
+# -- If a directory containing dunesetup.sh, then setup assuming build
+#    with [dune-dev](https://github.com/dladams/dune-dev) is done.
+# -- If a directory containing setup.sh, then that file is sourced.
+# -- If no '/' is found then dunesw version VERS with qualifier QUAL is set
+#    up from cvmfs. Thos values are obrtained assuming that REL is VERS:QUAL
+#    or VERS:DEFQUAL with the latter defined below (and subject to change).
 
-if [ -n "$VERS -a -n "$QUAL"" ]; then
-  source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh >/dev/null
-  setup dunesw $VERS -q $QUAL >/dev/null
-else
-  echo setup-dunesw.sh: ERROR: Invalid release:quailifier: $1
+DEFQUAL=e20:prof
+REL=$1
+
+DRELDIR=
+RELDIR=
+RELFIL=
+if grep / <(echo $REL) 2>&1 1>/dev/null; then
+  if [ -d $REL ]; then
+    if [ -r $REL/dunesetup.sh ]; then
+      DRELDIR=$REL
+    elif [ -r $REL/setup.sh ]; then
+      RELFIL=$REL/setup.sh
+    else
+      echo setup-dunesw.sh: ERROR: Not a build directory: $REL >&2
+    fi
+  else
+    if [ -r $REL ]; then
+      RELFIL=$REL
+    else
+      echo setup-dunesw.sh: ERROR: Setup file not found: $REL >&2
+    fi
+  fi
+  REL=
+fi
+
+if [ -n "$DRELDIR" ]; then
+  echo Setting up with dune-dev at $DRELDIR >&2
+  source $DRELDIR/dunesetup.sh
+  source $DRELDIR/run.sh
+elif [ -n "$RELFIL" ]; then
+  echo Setting up with $DRELDIR >&2
+  source $RELFIL
+elif [ -n "$REL" ]; then
+  VERS=$(echo $1 | sed 's/:.*//g')
+  QUAL=${1:$((${#VERS}+1))}
+  if [ -z "$QUAL" ]; then
+    QUAL=$DEFQUAL
+  fi
+  if [ -n "$VERS -a -n "$QUAL"" ]; then
+    echo Setting up dunesw $VERS $QUAL >&2
+    source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh >/dev/null
+    setup dunesw $VERS -q $QUAL >/dev/null
+  else
+    echo setup-dunesw.sh: ERROR: Invalid release:quailifier: $1 >&2
+  fi
 fi

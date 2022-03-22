ae07dcb6612d37a095172b7abe101b26b351cef4
David Adams <dladams@bnl.gov>
2022-03-22 13:33:25 +0000

    Rename local env var in setup-dunesw. 1.1.3

--- bin/setup-dunesw.sh
+++ bin/setup-dunesw.sh
@@ -4,40 +4,40 @@
 # March 2022
 #
 # Script to set up dunesw under different scenarios depending on the value
-# of the first positional argument REL. If this argument contains '/', it
+# of the first positional argument _REL. If this argument contains '/', it
 # is interpreted as a directory or file name. The action to take depends on
-# REL:
+# _REL:
 # -- If a file name ending '.sh', the file is sourced
 # -- If a directory containing dunesetup.sh, then setup assuming build
 #    with [dune-dev](https://github.com/dladams/dune-dev) is done.
 # -- If a directory containing setup.sh, then that file is sourced.
 # -- If no '/' is found then dunesw version VERS with qualifier QUAL is set
-#    up from cvmfs. Those values are obrtained assuming that REL is VERS:QUAL
+#    up from cvmfs. Those values are obrtained assuming that _REL is VERS:QUAL
 #    or VERS:DEFQUAL with the latter defined below (and subject to change).
 
 DEFQUAL=e20:prof
-REL=$1
+_REL=$1
 
 DRELDIR=
 RELDIR=
 RELFIL=
-if grep '/' <(echo $REL) 2>&1 1>/dev/null; then
-  if [ -d $REL ]; then
-    if [ -r $REL/dunesetup.sh ]; then
-      DRELDIR=$REL
-    elif [ -r $REL/setup.sh ]; then
-      RELFIL=$REL/setup.sh
+if grep '/' <(echo $_REL) 2>&1 1>/dev/null; then
+  if [ -d $_REL ]; then
+    if [ -r $_REL/dunesetup.sh ]; then
+      DRELDIR=$_REL
+    elif [ -r $_REL/setup.sh ]; then
+      RELFIL=$_REL/setup.sh
     else
-      echo setup-dunesw.sh: ERROR: Not a build directory: $REL >&2
+      echo setup-dunesw.sh: ERROR: Not a build directory: $_REL >&2
     fi
   else
-    if [ -r $REL ]; then
-      RELFIL=$REL
+    if [ -r $_REL ]; then
+      RELFIL=$_REL
     else
-      echo setup-dunesw.sh: ERROR: Setup file not found: $REL >&2
+      echo setup-dunesw.sh: ERROR: Setup file not found: $_REL >&2
     fi
   fi
-  REL=
+  _REL=
 fi
 
 if [ -n "$DRELDIR" ]; then
@@ -47,7 +47,7 @@ if [ -n "$DRELDIR" ]; then
 elif [ -n "$RELFIL" ]; then
   echo Setting up with $DRELDIR >&2
   source $RELFIL
-elif [ -n "$REL" ]; then
+elif [ -n "$_REL" ]; then
   VERS=$(echo $1 | sed 's/:.*//g')
   QUAL=${1:$((${#VERS}+1))}
   if [ -z "$QUAL" ]; then

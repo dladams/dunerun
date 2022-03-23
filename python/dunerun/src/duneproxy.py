f00ab7932651b7ec05f56bb2488adc7f98ab62eb
David Adams <dladams@bnl.gov>
2022-03-23 14:22:27 +0000

    Fix check and automate kinit in DuneProxy. 1.2.0
    The nopassword_kinit flag is dropped from DuneProxy.
    Instead a check for credentials is made and kinit run if they are
    not found. A message is broadcast is they are still not present.
    The flag '-s' is added in the credntitals check so that expired
    credentials will fail.

--- python/dunerun/src/duneproxy.py
+++ python/dunerun/src/duneproxy.py
@@ -18,20 +18,15 @@ class DuneProxy:
     @staticmethod
     def have_kerberos_credentials():
         """Returns true if the user has kerberos credentials."""
-        cpr = subprocess.run(['klist'], capture_output=True)
+        cpr = subprocess.run(['klist', '-s'], capture_output=True)
         return cpr.returncode == 0
 
-    def __init__(self, nopassword_kinit=False, tmin=60):
+    def __init__(self, tmin=60):
         """
         Class to check manage the user's VOMS proxy.
         Arguments:
-          nopassword_kinit - Set True if kerberos credentials can be obtained without
-                             user-suppplited password. In that case this class will
-                             generate those as needed. Otherwise the user is prompted
-                             to perform the kinit.
           tmin - Minimum remaining time [sec] for which is_valid returns True.
         """
-        self.nopassword_kinit = nopassword_kinit
         self.tmin = tmin
 
     def is_valid(self):
@@ -42,13 +37,16 @@ class DuneProxy:
         """
         Attempt to obtain a VOMS proxy. Returns if that proxy is valid.
         """
+        myname = 'DuneProxy:get_proxy'
         # We use DuneRun so we can kx509 from cvmfs.
-        # it is not installed at EAF.
+        # It is not installed at EAF.
         dune = dunerun.DuneRun('dune', shell=True, lev=lev)
+        # If kebereros credentials are not found, try kinit.
         if not self.have_kerberos_credentials():
-            if not self.nopassword_kinit:
-                raise Exception(f"{myname}: Kerberos credentials not found. Please run kinit.")
             dune.run('kinit')
+            if not self.have_kerberos_credentials():
+                print(f"{myname}: Unable to obtain Kerberos credentials. Please run kinit.")
+            return False
         dune.run('setup kx509')
         dune.run('kx509 --minhours 12')
         dune.run('voms-proxy-init -noregen -rfc -voms dune:/dune/Role=Analysis')

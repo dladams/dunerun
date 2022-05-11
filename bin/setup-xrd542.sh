741af77499401a41bdfe86fc1d513e167e4a3b43
GCO_admin <gco_admin@fnal.gov>
2022-05-11 16:15:02 +0000

    Add installation of xrootd libs.

--- bin/setup-xrd542.sh
+++ bin/setup-xrd542.sh
@@ -1,3 +1,30 @@
-export LD_LIBRARY_PATH=/dune/app/users/trj/xrootd542/install/lib64:${LD_LIBRARY_PATH}
-export LD_PRELOAD=/dune/app/users/trj/xrootd542/install/lib64/libXrdPosixPreload.so
+XRDIR_TOM=/dune/app/users/trj/xrootd542/install/lib64
+XRDIR_LOC=$HOME/lib/xrootd542/lib64
+
+if [ -r $XRDIR_TOM ]; then
+  XRDIR=$XRDIR_TOM
+elif [ -r $XRDIR_LOC ]; then
+  XRDIR=$XRDIR_LOC
+else
+  TARFIL=xrootd542.tar.gz
+  SRC=dunegpvm01.fnal.gov:$TARFIL
+  DST=$(dirname $XRDIR_LOC)
+  echo Copying $SRC to $DST
+  mkdir -p $DST
+  scp $SRC $DST
+  SAVEPWD=$(pwd)
+  cd $DST
+  echo Untarring
+  tar -zxf $TARFIL
+  cd $SAVEPWD
+  if [ -r $XRDIR_LOC ]; then
+    XRDIR=$XRDIR_LOC
+  else
+    echo Copy failed.
+    exit 1
+  fi
+fi
+
+export LD_LIBRARY_PATH=$XRDIR:${LD_LIBRARY_PATH}
+export LD_PRELOAD=$XRDIR/libXrdPosixPreload.so
 

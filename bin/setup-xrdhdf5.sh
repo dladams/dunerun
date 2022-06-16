0c6bc106aa9b8ec3631d05ef0fe18b9fb94c538b
GCO_admin <gco_admin@fnal.gov>
2022-06-16 13:45:52 +0000

    Fix setup-xrdhdf5.sh. 1.19.0

--- bin/setup-xrdhdf5.sh
+++ bin/setup-xrdhdf5.sh
@@ -1,12 +1,19 @@
-XRDIR_TOM=/dune/app/users/trj/xrootd542/install/lib64
-FLAV=$(basename $UPS_DIR)
-XRDIR_LOC=/cvmfs/dune.opensciencegrid.org/products/dune/testproducts/xrootd
-XROOTD_LIB=$(echo $XROOTD_LIB | sed "s#.*/products/xrootd/#$XRDIR_LOC/#g")
+# setup-xrdhdf5.sh
+#
+# Set up the test version of xrootd that has hdf5 streaming enabled.
+# We require the use have already set up xrootd and prepend streaming
+# libs the same version and flavor to LD_LIBRARYPATH and LD_PRELOAD.
 
-if [ ! -r $XROOTD_LIB ]; then
-  echo $MYNAME: ERROR: Directory not found: $XROOTD_LIB
+if [ -n "$XROOTD_LIB" ]; then
+  XRDIR_LOC=/cvmfs/dune.opensciencegrid.org/products/dune/testproducts/xrootd
+  XROOTD_LIB=$(echo $XROOTD_LIB | sed "s#.*/products/xrootd/#$XRDIR_LOC/#g")
+  if [ ! -r $XROOTD_LIB ]; then
+    echo setup-xrdhdf5: ERROR: Directory not found: $XROOTD_LIB >&2
+  else
+    export LD_LIBRARY_PATH=$XROOTD_LIB:${LD_LIBRARY_PATH}
+    export LD_PRELOAD=$XROOTD_LIB/libXrdPosixPreload.so:${LD_PRELOAD}
+  fi
 else
-  export LD_LIBRARY_PATH=$XROOTD_LIB:${LD_LIBRARY_PATH}
-  export LD_PRELOAD=$XROOTD_LIB/libXrdPosixPreload.so:${LD_PRELOAD}
+  echo setup-xrfhdf5: ERROR: Please first set up xrootd, e.g. from dunesw. >&2
 fi
 

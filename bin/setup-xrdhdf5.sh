6923035d129d088f802c500653582922395c2787
David Adams <dladams@bnl.gov>
2023-06-21 13:39:53 +0000

    Stopmtaking xrd hdf5 libs from testproducts. #6

--- bin/setup-xrdhdf5.sh
+++ bin/setup-xrdhdf5.sh
@@ -4,14 +4,14 @@
 # We require the use have already set up xrootd and prepend streaming
 # libs the same version and flavor to LD_LIBRARYPATH and LD_PRELOAD.
 
+LIBNAM=libXrdPosixPreload.so
 if [ -n "$XROOTD_LIB" ]; then
-  XRDIR_LOC=/cvmfs/dune.opensciencegrid.org/products/dune/testproducts/xrootd
-  XROOTD_LIB=$(echo $XROOTD_LIB | sed "s#.*/products/xrootd/#$XRDIR_LOC/#g")
   if [ ! -r $XROOTD_LIB ]; then
     echo setup-xrdhdf5: ERROR: Directory not found: $XROOTD_LIB >&2
+  elif [ ! -r $XROOTD_LIB/$LIBNAM ]; then
+    echo setup-xrdhdf5: ERROR: Library not found: $XROOTD_LIB/$LIBNAM >&2
   else
-    export LD_LIBRARY_PATH=$XROOTD_LIB:${LD_LIBRARY_PATH}
-    export LD_PRELOAD=$XROOTD_LIB/libXrdPosixPreload.so:${LD_PRELOAD}
+    export LD_PRELOAD=$XROOTD_LIB/$LIBNAM:${LD_PRELOAD}
   fi
 else
   echo setup-xrfhdf5: ERROR: Please first set up xrootd, e.g. from dunesw. >&2
